9cf57747f4f0435352c0820b9d0748f50b8c23af
Rafael V. de Oliveira <elrafael123@gmail.com>
2023-03-09 20:03:47 +0000

    custom events

--- note-app/src/App.vue
+++ note-app/src/App.vue
@@ -1,30 +1,57 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Note APP</h1>
+  <section class="columns">
+    <article class="notes">
+      <h1>Notes</h1>
+      <div v-for="note in notes">
+        {{ note }}
+      </div>
+    </article>
+
+    <article class="timestamp">
+      <h1>Timestamp: </h1>
+      <div v-for="ts in timestamps">
+        {{ ts }}
+      </div>
+    </article>
+  </section>
+
+  <InputNote
+    :placeholder="placeholder"
+    @add-note="addNote"
+  />
 </template>
 
+<script>
+import InputNote from './components/InputNote.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      notes: [],
+      timestamps: [],
+      placeholder: 'Enter note'
+    }
+  },
+  components: {
+    InputNote
+  },
+  methods: {
+    addNote(e) {
+      this.notes.push(e.note)
+      this.timestamps.push(e.timestamp)
+    }
+  }
+}
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+section.columns {
+  display: flex;
+  width: 600px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+section.columns > * {
+  flex: 1
 }
 </style>
---
+++ note-app/src/components/InputNote.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <input type="text" v-model="value" :placeholder="placeholder" @keyup.enter="monitorEnterKey">
+  </section>
+</template>
+<script>
+  export default {
+    name: 'InputNote',
+    data() {
+      return {
+        value: ''
+      }
+    },
+    props: [
+      'placeholder'
+    ],
+    emits: ['add-note'],
+    methods: {
+      monitorEnterKey() {
+        this.$emit('add-note', {
+          note: this.value,
+          timestamp: new Date().toLocaleDateString()
+        });
+        this.value = ''
+      }
+    }
+  }
+</script>
+<style scoped>
+input {
+  width: 90%;
+  border: 1px solid #ccc;
+  margin: 20px;
+  padding: 10px;
+}
+</style>
